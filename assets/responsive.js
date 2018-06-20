628763e2373b16f5f82387beda6575bad1947a1b
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-06-20 16:05:08 +0100

    Refactor menu code

--- assets/responsive.js
+++ assets/responsive.js
@@ -1,82 +1,71 @@
 /**
- * This function should be called when the mouse hovers over the menu.
+ * Open the menu.
  */
-function menuOver() {
-    if (!Modernizr.touch) {
-        $("#menu").addClass("mouseover")
+function menuOpen() {
+    $("#menu").addClass("mouseover")
+    $("#menu-button").addClass("mouseover")
+}
+
+
+/**
+ * Close the menu.
+ */
+function menuClose() {
+    $("#menu").removeClass("mouseover")
+    $("#menu-button").removeClass("mouseover")
+}
+
+
+/**
+ * Toggle the menu.
+ */
+function menuToggle() {
+    if ($("#menu").hasClass("mouseover")) {
+        menuClose()
+    } else {
+        menuOpen()
     }
 }
 
-/**
- * This function should be called when the mouse leaves the menu.
- */
-function menuOut() {
-    if (!Modernizr.touch) {
-        $("#menu").removeClass("mouseover")
-    }
-}
-
-var last_menu_click = (new Date()).getTime()  // Timestamp of last click
 
 /**
- * This function should be called when the user clicks the menu button.
+ * Fix the menu for touch devices. Makes the animations instant and brings menu
+ * button to the top.
  */
-function menuClick() {
-    if (Modernizr.touch) {
-        // Record click time.
-        last_menu_click = (new Date()).getTime()
+function menuTouch() {
+    $("#menu-button").css({
+        "z-index": 1,
+        "position": "absolute",
+        "transition": "background-image .2s"
+    })
 
-        // Toggle menu.
-        if ($("#menu").hasClass("mouseover")) {
-            $("#menu").removeClass("mouseover")
-        } else {
-            $("#menu").addClass("mouseover")
-        }
-    }
-}
-
-/**
- * This function should be called whenever the user clicks anywhere. Since the
- * body is a parent of the menu, this event should always come second.
- */
-function menuDocumentClick() {
-    if (Modernizr.touch) {
-        // Close menu if clicked anywhere else.
-        ms_since_last_menu_click = (new Date()).getTime() - last_menu_click
-        if (ms_since_last_menu_click > 100) {
-            $("#menu").removeClass("mouseover")
-        }
-    }
-}
-
-/**
- * This function should be called upon loading to fix the menu for touchscreen
- * devices.
- */
-function menuFix() {
-    if (Modernizr.touch) {
-        // Bring the menu button to the top.
-        menu_button = document.getElementById("menu-button")
-        menu_button.style.zIndex = 1
-        menu_button.style.position = "absolute"
-
-        // Make animation instant.
-        menu = document.getElementById("menu")
-        menu.style.transition = "width .2s, height .2s, background 0s ease .2s";
-    }
+    $("#menu").css({
+        "transition": "width .2s, height .2s, background .2s"
+    })
 }
 
 
 $(document).ready(function() {
-    // Load events.
-    $("#menu").mouseout(menuOut)
-    $("#menu").mouseover(menuOver)
-    $("#menu").click(menuClick)
-    $(document).click(menuDocumentClick)
-    //   Make it also work on touch devices.
-    $(document).on("click touchend", menuDocumentClick)
+    if (Modernizr.touch) {
+        menuTouch()
 
-    // Fix menu.
-    menuFix()
+        // Toggle menu upon click.
+        var menu_last_click = (new Date()).getTime()
+        $("#menu").click(function() {
+            menu_last_click = (new Date()).getTime()
+            menuToggle()
+        })
+
+        // Close menu if touched anywhere else.
+        $(document).on("click touchend", function() {
+            if ((new Date()).getTime() - menu_last_click > 100) {
+                menuClose()
+            }
+        })
+    } else {
+        // Simply toggle menu via hover.
+        $("#menu").mouseout(function() { menuClose() })
+        $("#menu").mouseover(function() { menuOpen() })
+    }
 })
 
