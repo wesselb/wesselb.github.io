c714985bc08598e436a3670d6082fc2339ce4f4f
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-01-16 10:11:55 +0100

    Add images for Stheno post

--- assets/images/posts/linear-models.py
+++ assets/images/posts/linear-models.py
@@ -1,3 +1,4 @@
+#! python
 from stheno import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,6 +6,9 @@ import wbml.plot
 
 
 wbml.plot.tex()
+
+
+B.set_random_seed(1)
 
 
 class Figure:
@@ -21,12 +25,12 @@ class Figure:
         plt.close()
 
 
-model = Measure()
+prior = Measure()
 x = np.linspace(0, 10, 100)
 
-a = GP(1, measure=model)
-b = GP(10, measure=model)
-noise = GP(Delta(), measure=model)
+a = GP(1, measure=prior)
+b = GP(10, measure=prior)
+noise = GP(Delta(), measure=prior)
 
 
 with Figure("constant-functions"):
@@ -55,8 +59,73 @@ with Figure("noisy-linear-functions-2"):
 
 with Figure("observations"):
     x_obs = np.linspace(0, 10, 50_000)
-    y_obs = y(x_obs).sample()
+    f_obs = 0.8 * x_obs - 2.5
+    y_obs = f_obs + 0.5 * np.random.randn(50_000)
     plt.scatter(x_obs, y_obs, edgecolor="none", s=4, alpha=0.2, c="black")
+
+
+post = prior | (y(x_obs), y_obs)
+pred = post(f(x_obs))
+
+print(repr(pred.mean))
+print(repr(pred.var))
+
+mean, lower, upper = pred.marginals()
+
+print(repr(mean))
+print(repr(upper - lower))
+
+print(repr(f_obs - mean))
+print(repr(np.mean((f_obs - mean) ** 2)))
+
+
+with Figure("denoised-observations"):
+    plt.scatter(x_obs, y_obs, edgecolor="none", s=4, alpha=0.2, c="black")
+    plt.plot(x_obs, mean, c="tab:blue", lw=3)
+
+
+def linear_model_denoise(x_obs, y_obs):
+    prior = Measure()
+    a = GP(1, measure=prior)            # Model for slope
+    b = GP(10, measure=prior)           # Model for offset
+    f = a * (lambda x: x) + b           # Noiseless linear model
+    noise = GP(Delta(), measure=prior)  # Model for noise
+    y = f + 0.5 * noise                 # Noisy linear model
+
+    post = prior | (y(x_obs), y_obs)    # Condition on observations.
+    pred = post(f(x_obs))               # Make predictions.
+    return pred.marginals()             # Return the mean and associated error bounds.
+
+
+
+print(repr(linear_model_denoise(x_obs, y_obs)))
+
+import jax
+import jax.numpy as jnp
+import stheno.jax
+
+try:
+    linear_model_denoise_jitted = jax.jit(linear_model_denoise)
+    print(linear_model_denoise(jnp.array(x_obs), jnp.array(y_obs)))
+except Exception as e:
+    print(e)
+
+control_flow_cache = B.ControlFlowCache()
+print(repr(control_flow_cache))
+with control_flow_cache:
+    linear_model_denoise(x_obs, y_obs)
+print(repr(control_flow_cache))
+
+
+@jax.jit
+def linear_model_denoise_jitted(x_obs, y_obs):
+    with control_flow_cache:
+        return linear_model_denoise(x_obs, y_obs)
+
+
+print(linear_model_denoise_jitted(jnp.array(x_obs), jnp.array(y_obs)))
+
+
 
 
 
